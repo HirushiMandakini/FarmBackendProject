fd22c1f0f133cdee945f5c06e6cc28108ac535ec
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-11-16 09:37:42 +0530

    add UserDAO and Mapping and AppUtil

--- src/main/java/com/example/farmBackend/FarmBackendApplication.java
+++ src/main/java/com/example/farmBackend/FarmBackendApplication.java
@@ -1,12 +1,22 @@
 package com.example.farmBackend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableMethodSecurity
 public class FarmBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FarmBackendApplication.class, args);
+	}
+	@Bean
+	public ModelMapper mapper(){
+		return new ModelMapper();
 	}
 }
--- src/main/java/com/example/farmBackend/dao/UserDAO.java
+++ src/main/java/com/example/farmBackend/dao/UserDAO.java
@@ -1,4 +1,10 @@
 package com.example.farmBackend.dao;
 
-public interface UserDAO {
+import com.example.farmBackend.entity.impl.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserDAO extends JpaRepository<Users, String> {
+    Optional<Users> findByEmail(String email);
 }
