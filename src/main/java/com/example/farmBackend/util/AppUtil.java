7189810e1076b117f506a04b72a90b3a9c3f5ce9
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-11-16 09:13:36 +0530

    add JWTAuth class and SignIn classes and add the generate userID part in AppUtil

--- src/main/java/com/example/farmBackend/util/AppUtil.java
+++ src/main/java/com/example/farmBackend/util/AppUtil.java
@@ -1,4 +1,9 @@
 package com.example.farmBackend.util;
 
+import java.util.UUID;
+
 public class AppUtil {
+    public static String generateUserID(){
+        return "USER-"+ UUID.randomUUID();
+    }
 }
