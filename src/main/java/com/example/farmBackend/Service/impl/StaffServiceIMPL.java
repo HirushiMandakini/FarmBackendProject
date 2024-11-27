e950a71192cb1e49e69379ae3a7f32d553b1f752
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-11-27 21:32:43 +0530

    add the StaffService and implement the IMPL class

---
+++ src/main/java/com/example/farmBackend/Service/impl/StaffServiceIMPL.java
@@ -0,0 +1,38 @@
+package com.example.farmBackend.Service.impl;
+
+import com.example.farmBackend.Service.StaffService;
+import com.example.farmBackend.dto.impl.StaffDTO;
+
+import java.util.List;
+
+public class StaffServiceIMPL implements StaffService {
+    @Override
+    public void saveStaff(StaffDTO staffDTO) {
+
+    }
+
+    @Override
+    public List<StaffDTO> getAllStaffs() {
+        return null;
+    }
+
+    @Override
+    public void deleteStaff(String id) {
+
+    }
+
+    @Override
+    public void updateStaff(String id, StaffDTO staffDTO) {
+
+    }
+
+    @Override
+    public List<StaffDTO> searchStaff(String searchTerm) {
+        return null;
+    }
+
+    @Override
+    public void returnVehicle(String staffId) {
+
+    }
+}
