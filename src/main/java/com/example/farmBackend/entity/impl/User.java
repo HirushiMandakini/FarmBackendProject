6a6803a0f6684c2777203da39f1c41e0fffec391
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-12-01 14:05:02 +0530

    added user Entity class

--- src/main/java/com/example/farmBackend/entity/impl/User.java
+++ src/main/java/com/example/farmBackend/entity/impl/User.java
@@ -1,11 +1,11 @@
-package org.example.cropmonitoringsystembackend.entity.impl;
+package com.example.farmBackend.entity.impl;
 
+import com.example.farmBackend.entity.SuperEntity;
+import com.example.farmBackend.util.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.example.cropmonitoringsystembackend.entity.SuperEntity;
-import org.example.cropmonitoringsystembackend.util.Role;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
