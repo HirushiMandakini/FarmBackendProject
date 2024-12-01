bb563b9efdea37428412db65706cc440b8b6e4e0
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-12-01 15:10:34 +0530

    added JWTService

--- src/main/java/com/example/farmBackend/Service/impl/JWTServiceIMPL.java
+++ src/main/java/com/example/farmBackend/Service/impl/JWTServiceIMPL.java
@@ -1,11 +1,11 @@
-package org.example.cropmonitoringsystembackend.service.impl;
+package com.example.farmBackend.Service.impl;
 
+import com.example.farmBackend.Service.JWTService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.example.cropmonitoringsystembackend.service.JWTService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
