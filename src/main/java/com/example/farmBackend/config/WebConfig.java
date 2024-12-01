451eafdf58031071264b9f7dfe04f10e8a5df8c5
HirushiMandakini <hirushimandakinimendis@gmail.com>
2024-12-01 14:01:56 +0530

    added UserDTO

--- src/main/java/com/example/farmBackend/config/WebConfig.java
+++ src/main/java/com/example/farmBackend/config/WebConfig.java
@@ -1,20 +1,16 @@
-package com.example.farmBackend.config;
+package org.example.cropmonitoringsystembackend.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
-
+public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-
 }
